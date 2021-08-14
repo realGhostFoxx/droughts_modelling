5ae5c500d38891c53e37b026559f07fe1d57bcba
Alexandru Stefanescu <stefanescu2244@gmail.com>
2021-08-14 13:19:26 +0300

    'initialcommit'

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import find_packages
+from setuptools import setup
+
+with open('requirements.txt') as f:
+    content = f.readlines()
+requirements = [x.strip() for x in content if 'git+' not in x]
+
+setup(name='droughts_modelling',
+      version="1.0",
+      description="Project Description",
+      packages=find_packages(),
+      install_requires=requirements,
+      test_suite='tests',
+      # include_package_data: to install data from MANIFEST.in
+      include_package_data=True,
+      scripts=['scripts/droughts_modelling-run'],
+      zip_safe=False)
