e906e5de349b1fa19a0a3cc0a3fcffcd6488db9b
Alexandru Stefanescu <stefanescu2244@gmail.com>
2021-08-24 22:03:53 +0300

    added aggregation function with mean as only calculation

--- droughts_modelling/data.py
+++ droughts_modelling/data.py
@@ -17,7 +17,6 @@ class DataFunctions:
     
         return temp_df
 
-
     def weekly_aggregate(self):
     
         file_path = os.path.dirname(os.path.dirname(os.path.dirname(os.getcwd())))
@@ -54,6 +53,50 @@ class DataFunctions:
                                          'WS50M_MAX': ['min', 'mean', 'std'],
                                          'WS50M_MIN': ['min', 'mean', 'std'],
                                          'WS50M_RANGE': ['min', 'mean', 'std'],
+                                         'score': 'max'}).reset_index().sort_values(['fips','year','week_num'])
+
+        #finally, remove the multiindex from aggregated data_train so it looks neat and has flat column name structure
+        #Then round scores to nearest integer
+        aggregated_data_train.columns = ['_'.join(col) for col in aggregated_data_train.columns.values]
+        aggregated_data_train['score_max'] = aggregated_data_train['score_max'].map(lambda x: np.round(x))
+        return aggregated_data_train
+    
+    def light_weekly_aggregate(self):
+    
+        file_path = os.path.dirname(os.path.dirname(os.path.dirname(os.getcwd())))
+        full_path = os.path.join(file_path,'realGhostFoxx','droughts_modelling', 'raw_data', 'train_timeseries.csv')
+        data = pd.read_csv(full_path)[2:]
+    
+        #first create new features: year, month, weekday, weeknum
+        data['week_num'] = pd.to_datetime(data['date']).dt.isocalendar().week
+        data['weekday'] = pd.to_datetime(data['date']).dt.weekday+1
+        data['month'] = pd.to_datetime(data['date']).dt.month_name()
+        data['year'] = pd.to_datetime(data['date']).dt.isocalendar().year
+
+        #then encode the score as a new feature - not sure if we'll need it 
+        data['score_day'] = data['score'].apply(lambda x: 'yes' if pd.notnull(x) == True else '')
+
+        #then start aggregating by fips, year, month, week_num
+        aggregated_data_train = data.groupby(['fips', 'year', 
+                                        'week_num']).agg(
+                                        {'PRECTOT': ['mean'],
+                                        'PS': ['mean'],
+                                        'QV2M': ['mean'],
+                                        'T2M': ['mean'],
+                                        'T2MDEW': ['mean'],
+                                        'T2MWET': ['mean'],
+                                        'T2M_MAX': ['mean'],
+                                        'T2M_MIN': ['mean'],
+                                        'T2M_RANGE': ['mean'],
+                                         'TS': ['mean'],
+                                         'WS10M': ['mean'],
+                                         'WS10M_MAX': ['mean'],
+                                         'WS10M_MIN': ['mean'],
+                                         'WS10M_RANGE': ['mean'],
+                                         'WS50M': ['mean'],
+                                         'WS50M_MAX': ['mean'],
+                                         'WS50M_MIN': ['mean'],
+                                         'WS50M_RANGE': ['mean'],
                                          'score': 'max'}).reset_index().sort_values(['fips','year','week_num'])
 
         #finally, remove the multiindex from aggregated data_train so it looks neat and has flat column name structure
