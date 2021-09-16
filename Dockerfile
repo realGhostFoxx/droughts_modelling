bc4a7de262248e73c95c362a40b52805db41477e
Jack Hooper <jackhooperaero@gmail.com>
2021-09-16 21:43:55 +0100

    final touches to app

--- Dockerfile
+++ Dockerfile
@@ -1,15 +1,11 @@
 FROM python:3.8.6-buster
 
-COPY api /api
-COPY api/fast.py /api/fast.py
-COPY api/__init__.py /api/__init__.py
 COPY droughts_modelling /droughts_modelling
 COPY requirements.txt /requirements.txt
-COPY Makefile /Makefile
-COPY setup.py /setup.py
-COPY api /api
-COPY raw_data /raw_data 
-
+COPY data /data 
+COPY app.py /app.py 
+COPY model /model 
+COPY crack_cracked_earth_2.jpg /crack_cracked_earth_2.jpg
 RUN pip install -r requirements.txt
 
-CMD uvicorn api.fast:app --host 0.0.0.0 --port $PORT
+CMD streamlit run app.py  --server.port 8080
--- app.py
+++ app.py
@@ -8,10 +8,15 @@ from droughts_modelling.window_gen import WindowGenerator
 from droughts_modelling.data import DataFunctions
 from sklearn.preprocessing import OneHotEncoder
 import base64
+import ast 
+
+def header(url):
+     st.markdown(f'<p style="background-color:#FFFFFF;color:#000000;font-size:32px;border-radius:2%;">{url}</p>', unsafe_allow_html=True)
 
 pages = ["Predict","About Project","Data Exploration"]
 page = st.sidebar.radio("Navigate", options=pages)
-st.title(page)
+#mapbox_api_key = os.getenv('MAPBOX_API_KEY')
+mapbox_api_key = "pk.eyJ1IjoianVsaW9lcTI5IiwiYSI6ImNrZTE0cG9tNzQyY2gycXR2eWVsbHlyd2cifQ.sx5Zm1WFmOJVbUnsT1q2aQ"
 
 main_bg = "crack_cracked_earth_2.jpg"
 main_bg_ext = "jpg"
@@ -33,35 +38,62 @@ st.markdown(
     unsafe_allow_html=True
 )
 
-file_path = os.path.dirname(os.path.dirname(os.path.dirname(os.getcwd())))
-full_path_fips = os.path.join(file_path,'code','realGhostFoxx','droughts_modelling', 'raw_data', 'fips_dict.csv')
+CSS = """
+h1 {
+    background-color: brown;
+    color: white;
+    padding: 12px;
+}
+
+p {
+    background-color: brown;
+    color: white;
+    padding: 12px;    
+}
+
+.row-widget {
+    background-color: grey;
+    color: white;
+    padding: 12px;    
+}
+"""
+st.write(f'<style>{CSS}</style>', unsafe_allow_html=True)
+
+header(page)
+
+
+file_path = os.path.dirname(os.getcwd())
+full_path_fips = os.path.join(file_path,'data', 'fips_dict.csv')
 fips_dict = pd.read_csv(full_path_fips,index_col=[0])
 
 if page == "Predict": 
     shift = 6
     inputs = 6
     
-    date = st.date_input("Date")
-    week = date.isocalendar()[1]
-    year = date.isocalendar()[0]  
-    
-    one_count = st.radio('Choose one county?',options=["yes","no"])
-    
+    st.write("Choose one county?")
+    one_count = st.radio("",options=["yes","no"])
+
     if one_count == "yes":
         fips_dict["UniqueCounty"] = fips_dict["COUNTYNAME"]+", "+fips_dict["STATE"]
-        county_string = st.selectbox(label="County",               
+        st.write("County:")
+        county_string = st.selectbox(label="",               
         options=fips_dict["UniqueCounty"],
         key=fips_dict["fips"]) 
         
         county = county_string[:-4]
         state = county_string[-2:]
         fips = fips_dict[fips_dict["STATE"]==state][fips_dict[fips_dict["STATE"]==state]["COUNTYNAME"] == county]["fips"].iloc[0]
-    
     else:
         fips = None   
         
+    
+    st.write("Date:")
+    date = st.date_input("")
+    week = date.isocalendar()[1]
+    year = date.isocalendar()[0]  
+    
     if (year <= 2018) | (year >= 2021):
-        st.write("Pick different date") 
+        st.write("Please select a starting date between 2019/01/01 and 2020/12/31") 
     
     else:                 
         if st.button('Predict'):
@@ -78,44 +110,53 @@ if page == "Predict":
             week_df = year_df[year_df["week_num_"] == week]
             
             predict_df = pd.DataFrame()
-            
-            st.write(week_df)
-            
+                        
             for index in week_df.index:
                 if inputs > (year-2019)*52+week:
                     st.write(f"Pick fewer inputs or later date")
                     predict_df = 0
                 else:
                     predict_df = predict_df.append(fips_df.loc[index-inputs+1:index+shift,:])
-                    
-            
-            st.write(predict_df)                    
-                
+                                    
             predict_df.drop(columns=["fips_","year_"],inplace=True)
             wingen = WindowGenerator(predict_df,input_width=inputs,label_width=1,shift=shift,label_columns=["score_max_0","score_max_1","score_max_2","score_max_3","score_max_4","score_max_5"])
             predict_data = wingen.split_window
             predict_data = wingen.make_dataset()      
             
-            model = tf.keras.models.load_model('trained_model_2021-09-11 13_01_35.778360.h5')
+            model = tf.keras.models.load_model("model/trained_model_2021-09-11 13_01_35.778360.h5")
             
             prediction = model.predict(predict_data)
             
             if fips:
                 likelihood = prediction[0][-1].max()
-                st.write(prediction[0][-1])
                 score = np.where(prediction[0][-1]==likelihood)
-                likelihood = round(likelihood,4)
+                likelihood = round(likelihood*100,2)
+
+                class_dict = {0:"no drought",
+                              1: "abnormally dry",
+                              2: "moderate drought",
+                              3: "severe drought",
+                              4: "extreme drought",
+                              5: "exceptional drought",}
                 
                 st.write(f"""On {predict_date} in {county} it is predicted 
-                that the drought classification will be {score[0][0]} with a """+str(likelihood)+f"% chance.")
+                that the drought classification will be {score[0][0]} - {class_dict[score[0][0]]} with a """+str(likelihood)+f"% chance.")              
+
+                latt_longt_str = fips_dict[fips_dict["fips"]==fips]["lat_long"].iloc[0]
+                latt_longt = ast.literal_eval(latt_longt_str)
+                longt = latt_longt[0]
+                latt = latt_longt[1]
+                lat_long_df = pd.DataFrame([[longt, latt]],columns=['lat', 'lon'])
+                st.map(lat_long_df)
+                
             else:
-                st.write(len(prediction))
                 score_list = []
                 for arr in prediction:
                     likelihood = arr[-1].max()
                     score = np.where(arr[-1]==likelihood)
                     score_list.append(score[0][0])
-                st.write(score_list)
+                st.image("exported_plots/2015-01-13.png",width=600)
+
                 
         else:
             pass
