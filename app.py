f574bada34e2d2328e794d45e171004f6d77b596
Julio Quintana <julioeq29@gmail.com>
2021-09-17 15:23:48 +0100

    corrected os cwd for reading path

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
 import os
 import streamlit as st
 import tensorflow as tf
-import pandas as pd 
+import pandas as pd
 import numpy as np
 import datetime
 from droughts_modelling.window_gen import WindowGenerator
 from droughts_modelling.data import DataFunctions
 from sklearn.preprocessing import OneHotEncoder
 import base64
-import ast 
+import ast
 
 def header(url):
      st.markdown(f'<p style="background-color:#FFFFFF;color:#000000;font-size:32px;border-radius:2%;">{url}</p>', unsafe_allow_html=True)
@@ -48,13 +48,13 @@ h1 {
 p {
     background-color: brown;
     color: white;
-    padding: 12px;    
+    padding: 12px;
 }
 
 .row-widget {
     background-color: grey;
     color: white;
-    padding: 12px;    
+    padding: 12px;
 }
 """
 st.write(f'<style>{CSS}</style>', unsafe_allow_html=True)
@@ -62,71 +62,71 @@ st.write(f'<style>{CSS}</style>', unsafe_allow_html=True)
 header(page)
 
 
-file_path = os.path.dirname(os.getcwd())
-full_path_fips = os.path.join(file_path,'data', 'fips_dict.csv')
+file_path = os.getcwd()
+full_path_fips = os.path.join(file_path, 'droughts_modelling', 'data', 'fips_dict.csv')
 fips_dict = pd.read_csv(full_path_fips,index_col=[0])
 
-if page == "Predict": 
+if page == "Predict":
     shift = 6
     inputs = 6
-    
+
     st.write("Choose one county?")
     one_count = st.radio("",options=["yes","no"])
 
     if one_count == "yes":
         fips_dict["UniqueCounty"] = fips_dict["COUNTYNAME"]+", "+fips_dict["STATE"]
         st.write("County:")
-        county_string = st.selectbox(label="",               
+        county_string = st.selectbox(label="",
         options=fips_dict["UniqueCounty"],
-        key=fips_dict["fips"]) 
-        
+        key=fips_dict["fips"])
+
         county = county_string[:-4]
         state = county_string[-2:]
         fips = fips_dict[fips_dict["STATE"]==state][fips_dict[fips_dict["STATE"]==state]["COUNTYNAME"] == county]["fips"].iloc[0]
     else:
-        fips = None   
-        
-    
+        fips = None
+
+
     st.write("Date:")
     date = st.date_input("")
     week = date.isocalendar()[1]
-    year = date.isocalendar()[0]  
-    
+    year = date.isocalendar()[0]
+
     if (year <= 2018) | (year >= 2021):
-        st.write("Please select a starting date between 2019/01/01 and 2020/12/31") 
-    
-    else:                 
+        st.write("Please select a starting date between 2019/01/01 and 2020/12/31")
+
+    else:
         if st.button('Predict'):
             predict_date = date+datetime.timedelta(weeks=shift)
             st.write(f'Future prediction date will be {predict_date}, which is {shift} weeks into the future.')
             test_df_robust_ohe = pd.read_csv("test_df_robust_ohe.csv",index_col=0)
-                  
-            if fips:                        
+
+            if fips:
                 fips_df = test_df_robust_ohe[test_df_robust_ohe["fips_"] == fips]
             else:
                 fips_df = test_df_robust_ohe
-            
+
             year_df = fips_df[fips_df["year_"] == year]
             week_df = year_df[year_df["week_num_"] == week]
-            
+
             predict_df = pd.DataFrame()
-                        
+
             for index in week_df.index:
                 if inputs > (year-2019)*52+week:
                     st.write(f"Pick fewer inputs or later date")
                     predict_df = 0
                 else:
                     predict_df = predict_df.append(fips_df.loc[index-inputs+1:index+shift,:])
-                                    
+
             predict_df.drop(columns=["fips_","year_"],inplace=True)
             wingen = WindowGenerator(predict_df,input_width=inputs,label_width=1,shift=shift,label_columns=["score_max_0","score_max_1","score_max_2","score_max_3","score_max_4","score_max_5"])
             predict_data = wingen.split_window
-            predict_data = wingen.make_dataset()      
-            
+            predict_data = wingen.make_dataset()
+
             model = tf.keras.models.load_model("model/trained_model_2021-09-11 13_01_35.778360.h5")
-            
+
             prediction = model.predict(predict_data)
-            
+
             if fips:
                 likelihood = prediction[0][-1].max()
                 score = np.where(prediction[0][-1]==likelihood)
@@ -138,9 +138,9 @@ if page == "Predict":
                               3: "severe drought",
                               4: "extreme drought",
                               5: "exceptional drought",}
-                
-                st.write(f"""On {predict_date} in {county} it is predicted 
-                that the drought classification will be {score[0][0]} - {class_dict[score[0][0]]} with a """+str(likelihood)+f"% chance.")              
+
+                st.write(f"""On {predict_date} in {county} it is predicted
+                that the drought classification will be {score[0][0]} - {class_dict[score[0][0]]} with a """+str(likelihood)+f"% chance.")
 
                 latt_longt_str = fips_dict[fips_dict["fips"]==fips]["lat_long"].iloc[0]
                 latt_longt = ast.literal_eval(latt_longt_str)
@@ -148,7 +148,7 @@ if page == "Predict":
                 latt = latt_longt[1]
                 lat_long_df = pd.DataFrame([[longt, latt]],columns=['lat', 'lon'])
                 st.map(lat_long_df)
-                
+
             else:
                 score_list = []
                 for arr in prediction:
@@ -157,21 +157,21 @@ if page == "Predict":
                     score_list.append(score[0][0])
                 st.image("exported_plots/2015-01-13.png",width=600)
 
-                
+
         else:
             pass
-        
+
 elif page == "About Project":
    st.write("The aim of this project was to build a Deep Learning model to predict future occurences of drought using meteorological time series data. The training data we used comprised of 3100 simultaneous sequences, each of 16 years, each belonging to a US county, simply stacked on top of eachother in a c.17,000,000 row CSV file.")
-   
+
    st.write("The model used is a Long Short Term Memory (LSTM) model - a kind of recurrent neural network well suited to time series data. The model has been trained on countless data windows, configured in such a way as to allow it to predict as far as X weeks into the future, using X weeks of historical data to do so." )
-   
+
    st.write("Currently the model is predicting with an estimated accuracy of X, a fair score considering the complexity of the task and the short period of time it was built in!")
-   
+
    st.write("The dataset and the original kaggle post that inspired this project can be found here: https://www.kaggle.com/cdminix/us-drought-meteorological-data")
-   
+
 else:
-    pass 
+    pass
 
 
 
